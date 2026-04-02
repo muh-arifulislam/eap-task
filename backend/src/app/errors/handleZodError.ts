890d0337001e47b352a891061c1e83bb8673c918
muh-arifulislam <arifibnenam@gmail.com>
2026-04-02 10:07:35 -0600

    restarted working on frontend after accidentaly deleting from local and no backup in git

--- backend/src/app/errors/handleZodError.ts
+++ backend/src/app/errors/handleZodError.ts
@@ -3,16 +3,24 @@ import { TErrorSources, TGenericErrorResponse } from "../types/error";
 
 const handleZodError = (err: ZodError): TGenericErrorResponse => {
   const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+    // Take last path if exists, otherwise use "root"
+    const lastPath =
+      issue.path.length > 0 ? issue.path[issue.path.length - 1] : "root";
+
+    // Ensure type is string | number
+    const path: string | number =
+      typeof lastPath === "string" || typeof lastPath === "number"
+        ? lastPath
+        : String(lastPath);
+
     return {
-      path: issue?.path[issue.path.length - 1],
+      path,
       message: issue.message,
     };
   });
 
-  const statusCode = 400;
-
   return {
-    statusCode,
+    statusCode: 400,
     message: "Validation Error",
     errorSources,
   };
